67fe6b0a4c4810c5e5c84ef9bdef1a852c8eee03
mounirkanane <71102660+MounirKanane@users.noreply.github.com>
2024-12-29 21:46:43 -0500

    tailwind setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,10 @@
 import '@/styles/globals.css'
+import {Monteserrat} from "next/font/google"
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont"
+})
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
