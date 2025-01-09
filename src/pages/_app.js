beb68d0daadc79219d56c68a3583c36b20cda3bf
Mounir <kanane.mounir@gmail.com>
2025-01-09 07:00:44 -0500

    NavBar complete

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,25 @@
-import '@/styles/globals.css'
-import {Monteserrat} from "next/font/google"
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+import Head from "next/head";
+import NavBar from "@/components/NavBar";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
-  variable: "--font-mont"
-})
+  display: "swap",
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${montserrat.variable} bg-light w-full min-h-screen`}>
+        <NavBar />
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199 (dark pink)
+        primaryDark: "#58E6D9", // 80,230,217 (electric blue)
+      },
+    },
   },
   plugins: [],
-}
-
+};
